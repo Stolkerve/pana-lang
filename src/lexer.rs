39b2c79732e54b8332f4e391e928600787338953
Stolkerve <devsebasgr@gmail.com>
2023-09-27 23:00:33 -0400

    Arreglar un bug con el lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -282,7 +282,7 @@ impl Lexer {
             '-' => Token::new(TokenType::Minus, self.line, self.col),
             '/' => Token::new(TokenType::Slash, self.line, self.col),
             '*' => Token::new(TokenType::Asterisk, self.line, self.col),
-            '!' => self.read_2chars_token('=', TokenType::Eq, TokenType::Bang),
+            '!' => self.read_2chars_token('=', TokenType::NotEq, TokenType::Bang),
             '<' => self.read_2chars_token('=', TokenType::LtEq, TokenType::Lt),
             '>' => self.read_2chars_token('=', TokenType::GtEq, TokenType::Gt),
             ',' => Token::new(TokenType::Comma, self.line, self.col),
